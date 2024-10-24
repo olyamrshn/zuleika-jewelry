06b87205437d901262fcc7ef187a5d6b9b1bb92d
marshennikovaolga <marshennikova@gmail.com>
2024-10-24 16:05:33 +0200

    .

--- app/routes/collection/eden.tsx
+++ app/routes/collection/eden.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { Link } from "@tanstack/react-router"
 
 const products = [
   {
@@ -38,13 +39,18 @@ export const Route = createFileRoute("/collection/eden")({
     <div className="p-4 text-center min-h-screen">
       <h1 className="text-lg font-semibold mb-4">EDEN</h1>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-1">
-        {products.map((product, index) => (
-          <div key={index} className="p-2">
-            <img src={product.image} className="w-full h-auto aspect-square" />
-            <div className="text-left">
-              <p className="text-[13px]">{product.name}</p>
-              <span className="text-xs opacity-55">{product.price}</span>
-            </div>
+        {products.map((product) => (
+          <div key={product.name} className="p-2">
+            <Link to="/product">
+              <img
+                src={product.image}
+                className="w-full h-auto aspect-square"
+              />
+              <div className="text-left">
+                <p className="text-[13px]">{product.name}</p>
+                <span className="text-xs opacity-55">{product.price}</span>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
--- app/routes/product.tsx
+++ app/routes/product.tsx
@@ -4,69 +4,55 @@ export const Route = createFileRoute("/product")({
   component: () => (
     <div className="min-h-screen p-4">
       <div className="flex flex-col lg:flex-row gap-3 max-w-screen justify-center mx-auto">
-        <div className="w-[50%]">
+        <div className="w-[48%]">
           <img
             src="https://images.omrshn.dev/collection.jpeg"
             alt="Niloufer Luna Necklace"
             className="w-full h-auto object-contain"
           />
         </div>
-        <div className="flex flex-row w-1/2">
-          <div>
-            <div className="space-y-4">
-              <h1 className="text-md font-semibold">
+        <div className="flex flex-row space-x-6 w-1/2">
+          <div className="w-1/2">
+            <div>
+              <h1 className="text-md font-semibold w-[85%]">
                 Niloufer Luna Necklace 40cm in yellow gold and pearls
               </h1>
-              <p className="text-xl">$0 000</p>
+              <p className="text-sm my-4 opacity-55">$0 000</p>
             </div>
 
-            <p className="text-gray-600">
+            <p className="opacity-50 text-sm  leading-relaxed">
               Take a classic that has stood the test of time, then pull it and
               twist it. Make people look twice. Smooth pearls are interspersed
               with handmade granulated gold cones, strikingly framing the face.
             </p>
 
-            <div className="space-y-4">
+            <div className="space-y-4 my-5">
               <div>
                 <h2 className="text-sm mb-2">Size:</h2>
                 <div className="flex gap-2">
-                  <button className="border-2 border-black rounded-none w-full">
-                    One Size
-                  </button>
+                  <p className="text-sm">One Size</p>
                 </div>
               </div>
 
-              <button className="w-full p-2 py-3 border-2 border-black rounded-none">
+              <button className="w-[90%] mx-auto text-sm my-3 p-2 py-3 border-2 border-black rounded-none">
                 Add to bag
               </button>
             </div>
-            <div className="grid grid-cols-2 gap-4 pt-6">
-              <button className="text-gray-600 text-left text-sm">
-                Description
-              </button>
-              <button className="text-gray-600 text-right text-sm">
-                Need assistance?
-              </button>
-              <button className="text-gray-600 text-left text-sm">
-                About the collection
-              </button>
-              <button className="text-gray-600 text-right text-sm">
-                Shipping
-              </button>
-              <button className="text-gray-600 text-left text-sm">
-                Savoir faire
-              </button>
-              <button className="text-gray-600 text-right text-sm">
-                Care advice
-              </button>
+            <div className="grid grid-cols-2 text-sm opacity-55 gap-4 pt-6">
+              <button className="text-left">Description</button>
+              <button className="text-left">Need assistance?</button>
+              <button className="text-left">About the collection</button>
+              <button className="text-left">Shipping</button>
+              <button className="text-left">Savoir faire</button>
+              <button className="text-left">Care advice</button>
             </div>
           </div>
 
-          <div>
+          <div className="w-1/2 text-sm">
             <div className="space-y-6 pt-6">
               <div>
-                <h2 className="font-medium text-md mb-2">Description</h2>
-                <p className="text-sm text-gray-600">
+                <h2 className="font-medium mb-2">Description</h2>
+                <p className="opacity-50 leading-relaxed">
                   A 40cm string of 8mm round pearls, interspersed with 9
                   granulated gold cone caps that fit snugly around the pearls.
                   The lock detail has been considered with care, a granulated
@@ -76,7 +62,7 @@ export const Route = createFileRoute("/product")({
 
               <div>
                 <h2 className="font-medium mb-2">Materials</h2>
-                <p className="text-sm text-gray-600">
+                <p className="opacity-50 text-sm  leading-relaxed">
                   Yellow gold 18k, 8mm white pearls
                 </p>
               </div>
