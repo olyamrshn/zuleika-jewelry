847476b8112a0b83d884d0ecbe7374f0eb9ce4a3
marshennikovaolga <marshennikova@gmail.com>
2024-10-24 13:51:12 +0200

    .

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -103,46 +103,70 @@ export default function Nav() {
           </div>
         </div>
       ) : (
-        <>
-          <div className="flex space-x-4 mr-auto">
-            <a href="#">FR</a>
-            <span>|</span>
-            <a href="#">EN</a>
+        <nav className="flex flex-col lg:flex-row items-end p-4 px-5 zuleika-text-color font-kepler">
+          <div className="flex justify-between lg:justify-start w-full lg:mb-4 order-1 lg:order-none">
+            <div className="flex space-x-4">
+              <a href="#">FR</a>
+              <span>|</span>
+              <a href="#">EN</a>
+            </div>
+            <div className="flex lg:hidden space-x-4">
+              <span>* | * | * | *</span>
+            </div>
           </div>
-          <div className="flex space-x-8 font-bold items-center">
-            <Link to="/jewellery" className="mb-1 font-thin">
-              JEWELLERY
-            </Link>
-            <p className="font-thin"> — </p>
-            <Link to="/" className="mb-1">
-              E-STORE
-            </Link>
+
+          <div className="text-center flex flex-col w-full lg:w-auto font-keplerCondensed order-0 lg:order-none mb-4 lg:mb-0">
             <Link
               to="/"
-              className="text-2xl tracking-[0.2em] transform scale-y-150"
+              className="text-4xl mb-6 lg:mb-0"
+              style={{ lineHeight: "2", letterSpacing: "0.1em" }}
             >
               ZULEIKA
             </Link>
-            <Link to="/atelier" className="mb-1">
-              ATELIER
-            </Link>
+            <div className="flex flex-col lg:flex-row justify-center text-sm lg:space-x-10 space-y-4 lg:space-y-0">
+              <Link to="/jewellery" className="relative group inline-block">
+                <span>STORE</span>
+                <span className="absolute left-0 -bottom-0.5 w-full h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+              </Link>
+              <Link
+                to="/bijoux-despace"
+                className="relative group inline-block"
+              >
+                <span>BIJOUX&nbsp;D'ESPACE</span>
+                <span className="absolute left-0 -bottom-0.5 w-full h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+              </Link>
+              <Link to="/atelier" className="relative group inline-block">
+                <span className="absolute left-0 -bottom-0.5 w-full h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+                <span>ATELIER</span>
+              </Link>
+              <Link to="/about" className="relative group inline-block">
+                <span className="absolute left-0 -bottom-0.5 w-full h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+                <span>ABOUT</span>
+              </Link>
+              <Link to="/contact" className="relative group inline-block">
+                <span>CONTACT</span>
+                <span className="absolute left-0 -bottom-0.5 w-full h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+              </Link>
+            </div>
           </div>
-          <div className="flex space-x-8 font-normal items-center">
-            <p> — </p>
-            <Link to="/bijoux-despace" className="ml-2 relative group">
-              <span>BIJOUX D'ESPACE</span>
-              <span className="absolute left-0 -bottom-0.5 w-full h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
-            </Link>
-            <Link to="/about" className="ml-2 relative group">
-              <span>ABOUT</span>
-              <span className="absolute left-0 -bottom-0.5 w-full h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
-            </Link>
-            <Link to="/contact" className="ml-2 relative group">
-              <span>CONTACT</span>
-              <span className="absolute left-0 -bottom-0.5 w-full h-px bg-current transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
-            </Link>
+
+          <div className="hidden lg:flex justify-end w-full lg:mb-4 order-2 lg:order-none mt-4 lg:mt-0">
+            <div className="flex space-x-4 text-neutral-500">
+              <button>
+                <SearchIcon />
+              </button>
+              <button>
+                <UserIcon />
+              </button>
+              <button>
+                <HeartIcon />
+              </button>
+              <button>
+                <BagIcon />
+              </button>
+            </div>
           </div>
-        </>
+        </nav>
       )}
     </div>
   )
