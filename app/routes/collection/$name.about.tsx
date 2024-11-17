c7ee4f5b9efeb53501281ac1607aa322187d1bd9
marshennikovaolga <marshennikova@gmail.com>
2024-11-17 12:59:42 +0100

    .

--- app/routes/collection/$name.about.tsx
+++ app/routes/collection/$name.about.tsx
@@ -3,26 +3,28 @@ import CollectionNav from "~/components/CollectionNav"
 import { collections, type CollectionName } from "~/data/collections"
 import { NotFound } from "~/components/NotFound"
 
+function RouteComponent() {
+  const { name } = Route.useParams()
+  const collection = collections[name as CollectionName]
+  if (!collection) {
+    return <NotFound />
+  }
+
+  return (
+    <div className="p-4 text-center min-h-screen">
+      <h1 className="text-lg font-semibold mb-4">{collection.about.title}</h1>
+      <div>RENDER .....</div>
+      <div className="max-w-2xl mx-auto mt-8">
+        <p className="text-left">{collection.about.description}</p>
+      </div>
+    </div>
+  )
+}
+
 export const Route = createFileRoute("/collection/$name/about")({
   parseParams: (params: Record<string, string>): { name: CollectionName } => {
     const name = params.name as CollectionName
     return Object.keys(collections).includes(name) ? { name } : { name: "eden" }
   },
-  component: ({ params }) => {
-    const collection = collections[params.name as CollectionName]
-
-    if (!collection) {
-      return <NotFound />
-    }
-
-    return (
-      <div className="p-4 text-center min-h-screen">
-        <h1 className="text-lg font-semibold mb-4">{collection.about.title}</h1>
-        <CollectionNav collectionName={params.name} />
-        <div className="max-w-2xl mx-auto mt-8">
-          <p className="text-left">{collection.about.description}</p>
-        </div>
-      </div>
-    )
-  },
+  component: RouteComponent,
 })
--- app/routes/collection/$name.tsx
+++ app/routes/collection/$name.tsx
@@ -4,47 +4,49 @@ import CollectionNav from "~/components/CollectionNav"
 import { collections, type CollectionName } from "~/data/collections"
 import { NotFound } from "~/components/NotFound"
 
+function RouteComponent() {
+  const { name } = Route.useParams()
+  console.log("runs..")
+
+  if (!name) {
+    return <NotFound />
+  }
+
+  const collection = collections[name as CollectionName]
+
+  if (!collection) {
+    return <NotFound />
+  }
+
+  return (
+    <div className="p-4 text-center min-h-screen">
+      <h1 className="text-lg font-semibold mb-4">{name.toUpperCase()}</h1>
+      <CollectionNav collectionName={name} />
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-1">
+        {collection.products.map((product) => (
+          <div key={product.name} className="p-2">
+            <Link to="/product">
+              <img
+                src={product.image}
+                className="w-full h-auto aspect-square"
+              />
+              <div className="text-left">
+                <p className="text-[13px]">{product.name}</p>
+                <span className="text-xs opacity-55">{product.price}</span>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
 export const Route = createFileRoute("/collection/$name")({
   parseParams: (params: { name: string }): { name: CollectionName } => {
     const name = params.name as CollectionName
     console.log("Parsing params:", params)
     return Object.keys(collections).includes(name) ? { name } : { name: "eden" }
   },
-  component: ({ params }) => {
-    console.log("Received params:", params)
-    if (!params.name) {
-      return <NotFound />
-    }
-
-    const collection = collections[params.name as CollectionName]
-
-    if (!collection) {
-      return <NotFound />
-    }
-
-    return (
-      <div className="p-4 text-center min-h-screen">
-        <h1 className="text-lg font-semibold mb-4">
-          {params.name.toUpperCase()}
-        </h1>
-        <CollectionNav collectionName={params.name} />
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-1">
-          {collection.products.map((product) => (
-            <div key={product.name} className="p-2">
-              <Link to="/product">
-                <img
-                  src={product.image}
-                  className="w-full h-auto aspect-square"
-                />
-                <div className="text-left">
-                  <p className="text-[13px]">{product.name}</p>
-                  <span className="text-xs opacity-55">{product.price}</span>
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div>
-    )
-  },
+  component: RouteComponent,
 })
